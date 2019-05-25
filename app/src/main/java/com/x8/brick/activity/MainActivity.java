9814f332296a04d81d2d9727869055c6a4838df5
方块8 <528787067@qq.com>
2019-05-26 03:44:52 +0800

    [init] 初始化项目

---
+++ app/src/main/java/com/x8/brick/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.x8.brick.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.x8.brick.R;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main_activity);
+	}
+}
